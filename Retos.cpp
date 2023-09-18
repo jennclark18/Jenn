31bd973c17cfecc8940680d9e95474500337730d
Jenn <144074177+jennclark18@users.noreply.github.com>
2023-09-18 10:34:16 -0600

    Add files via upload

--- Retos.cpp
+++ Retos.cpp
@@ -3,17 +3,15 @@
 
 #include <stdio.h>
 
-int main() 
+int main()
 {
-    float lado, area;
-
-    printf("Ingrese la longitud del lado del cuadrado: ");
-    scanf("%f", &lado);
-
-    // Calcula el área del cuadrado
-    area = lado * lado;
-
-    printf("El área del cuadrado es: %.2f\n", area);
-
-    return 0;
+    int area;
+    int altura;
+    int base;
+    printf("Altura:");
+    scanf_s("%i", &altura);
+    printf("Base:");
+    scanf_s("%i", &base);
+    area = base * altura;
+    printf("El cuadrado de %i x %i tiene area de %i", base, altura, area);
 }
