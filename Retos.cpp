d70623cfa06760b826c15c72ba8387e28ee11a1a
Jenn <144074177+jennclark18@users.noreply.github.com>
2023-09-18 10:43:18 -0600

    V3 circulo

--- Retos.cpp
+++ Retos.cpp
@@ -5,13 +5,18 @@
 
 int main()
 {
-    int area;
-    int altura;
-    int base;
-    printf("Altura:");
-    scanf_s("%i", &altura);
-    printf("Base:");
-    scanf_s("%i", &base);
-    area = base * altura;
-    printf("El cuadrado de %i x %i tiene area de %i", base, altura, area);
+    // Definir una variable para almacenar el Radio del Circulo
+    double Radio;
+
+    // Solicitar al usuario que ingrese el Radio del Circulo
+    printf("Ingrese el Radio del Circulo: ");
+    scanf_s("%lf", &Radio);
+
+    // Calcular el Area del Circulo usando la formula: Area = pi * Radio * Radio
+    double Area = 3.14159265359 * Radio * Radio;
+
+    // Mostrar el resultado
+    printf("El Area del Circulo con radio %.2lf es %.2lf\n", Radio, Area);
+
+    return 0;
 }
